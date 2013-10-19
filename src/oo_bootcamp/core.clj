e4bbed3ae9642b90661c353cc12fa15d74aae455
Suo Qin <kpsesuo@gmail.com>
2013-10-19 23:35:22 +0800

    format code, add comments

--- src/oo_bootcamp/core.clj
+++ src/oo_bootcamp/core.clj
@@ -1,14 +1,32 @@
 (ns oo-bootcamp.core
-  (:gen-class))
+  (:gen-class ))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
 
-(defn ticket [index] (fn [lot] (nth lot index)))
-(defn park [car lot] [(concat lot [car]) (ticket (count lot))])
+(defn ticket
+  "ticket of current parking"
+  [index]
+  (fn [lot] (nth lot index)))
 
-(defn myticket [parked] (last parked))
-(defn theParkingLot [parked] (first parked))
-(defn takeCarFrom [parked] ((myticket parked)(theParkingLot parked)))
+(defn park
+  "to park a car in the parking lot"
+  [car lot]
+  [(concat lot [car]) (ticket (count lot))])
+
+(defn myticket
+  "get ticket from last parking"
+  [parked]
+  (last parked))
+
+(defn theParkingLot
+  "get lot from last parking"
+  [parked]
+  (first parked))
+
+(defn takeCarFrom
+  "take car away"
+  [parked]
+  ((myticket parked) (theParkingLot parked)))
